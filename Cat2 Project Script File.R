8ec31baa302b00c965800a90ec1fe0f7c089f619
Talha318 <talha318@live.com>
2017-05-21 15:17:54 -0500

    updated script

--- Cat2 Project Script File.R
+++ Cat2 Project Script File.R
@@ -1,13 +1,43 @@
+#install necessary packages
+require(qdap)
+require(dplyr)
+require(caTools)
+
 #Loading Raw data set
 
 MasterData = read.csv("Raw Learning Data.csv" , header=TRUE)
-RawData = MasterData
+df = MasterData
 
+#convert data into character type
+df$Text = as.character(df$Text)
+df$Identifier = as.character(df$Identifier)
+df$Level = as.character(df$Level)
+df$Category = as.factor(df$Category)   #"Category" needs to be factor for easy manipulation
 
-for 1 : length(levels(RawData$Category)) 
-{    
+#Handing Duplicates in identifier
   
-SummaryStats[i] = summary(RawData$Category)[i] / 34233
+  #identifing dupilicates
+  TextDup = df$Text[which(duplicated(df$Identifier))]
+
+  #removing duplicates in identifier
+  df = df[-c(which(duplicated(df$Identifier))), ]
 
   
-}
+#word count of regulation text
+df$wordcount = wc(df$Text)
+
+#Splitting Data into testing and training sets
+set.seed(123)
+dt = sort(sample(nrow(df), nrow(df)*.7))
+dfTr = df[dt,] 
+Test = df[-dt,]
+
+
+#subsetting separating categories
+dfActionable = subset(dfTr, Category == "Actionable") 
+dfDef = subset(dfTr, Category == "NA - Definition") 
+dfOther = subset(dfTr, Category == "NA - Other") 
+dfTrig = subset(dfTr, Category == "Trigger") 
+dfUnCat = subset(dfTr, Category == "")
+
+  
