e9ccec199ab453bc1858c75996ddfddc98628aa9
Talha H <talha318@live.com>
2017-06-21 21:19:51 -0500

    Add files via upload
    updated script to start support vector machines test run - not complete yet.

--- Cat2 Project Script File.R
+++ Cat2 Project Script File.R
@@ -2,6 +2,7 @@
 require(qdap)
 require(dplyr)
 require(caTools)
+require(stringr)
 
 #Loading Raw data set
 
@@ -14,30 +15,39 @@ df$Identifier = as.character(df$Identifier)
 df$Level = as.character(df$Level)
 df$Category = as.factor(df$Category)   #"Category" needs to be factor for easy manipulation
 
-#Handing Duplicates in identifier
-  
-  #identifing dupilicates
-  TextDup = df$Text[which(duplicated(df$Identifier))]
+#Converting all text to lower case
+df$Text = str_to_lower(df$Text, locale = "en")
 
-  #removing duplicates in identifier
-  df = df[-c(which(duplicated(df$Identifier))), ]
+#identifing dupilicates
+TextDup = df$Text[which(duplicated(df$Identifier))]
 
-  
+#removing duplicates in identifier
+df = df[-c(which(duplicated(df$Identifier))), ]
+
+#Renumbering rows in data set
+row.names(df) = 1:nrow(df)
+
 #word count of regulation text
-df$wordcount = wc(df$Text)
+df$wordcount = str_count(df$Text, "\\S+")
 
 #Splitting Data into testing and training sets
-set.seed(123)
-dt = sort(sample(nrow(df), nrow(df)*.7))
-dfTr = df[dt,] 
-Test = df[-dt,]
+#set.seed(123)
+#dt = sort(sample(nrow(df), nrow(df)*.7))
+#dfTr = df[dt,] 
+#Test = df[-dt,]
+dfTr = df
 
+#Creating Features / key words and phrases that suggest actionability
+df$shall = str_detect(df[,3],"shall")
+df$shall_submit = str_detect(df[,3],"shall submit")
+df$permittee_shall = str_detect(df[,3],"permittee shall")
+df$permittee_must = str_detect(df[,3],"permittee must")
+df$owner_or_operator = str_detect(df[,3],"owner or operator")
+df$owner_operator = str_detect(df[,3],"owner/operator")
 
-#subsetting separating categories
+#Subsetting separating categories
 dfActionable = subset(dfTr, Category == "Actionable") 
 dfDef = subset(dfTr, Category == "NA - Definition") 
 dfOther = subset(dfTr, Category == "NA - Other") 
 dfTrig = subset(dfTr, Category == "Trigger") 
 dfUnCat = subset(dfTr, Category == "")
-
-  
