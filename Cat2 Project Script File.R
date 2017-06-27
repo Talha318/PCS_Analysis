0d0f883512e8021dd66f8f983c7f504f9b698610
Talha H <talha318@live.com>
2017-06-27 12:06:13 -0500

    Add files via upload
    Updated Script file

--- Cat2 Project Script File.R
+++ Cat2 Project Script File.R
@@ -3,22 +3,25 @@ require(qdap)
 require(dplyr)
 require(caTools)
 require(stringr)
+library(tm)
 
 #Loading Raw data set
-
-MasterData = read.csv("Raw Learning Data.csv" , header=TRUE)
+MasterData = read.csv("Raw Learning Data.csv" , header=TRUE, stringsAsFactors = FALSE, encoding = "UTF-8")
 df = MasterData
+
+
 
 #convert data into character type
 df$Text = as.character(df$Text)
 df$Identifier = as.character(df$Identifier)
 df$Level = as.character(df$Level)
 df$Category = as.factor(df$Category)   #"Category" needs to be factor for easy manipulation
+df$Category [df$Category != "Actionable"] = "NA - Other" #turning all non-actionable categorizations to na-other for binary classification
 
 #Converting all text to lower case
 df$Text = str_to_lower(df$Text, locale = "en")
 
-#identifing dupilicates
+#identifing duplicates
 TextDup = df$Text[which(duplicated(df$Identifier))]
 
 #removing duplicates in identifier
@@ -35,19 +38,35 @@ df$wordcount = str_count(df$Text, "\\S+")
 #dt = sort(sample(nrow(df), nrow(df)*.7))
 #dfTr = df[dt,] 
 #Test = df[-dt,]
-dfTr = df
+
+#set a raw text data frame
+Text = iconv(df$Text, "UTF-8", "UTF-8",sub="")
+Text = as.data.frame(Text)  
+
+#Cleaned Data to create Corpus
+dfcorpus = Corpus(DataframeSource(Text))
+
+#removing numbers, punctuation, and english stop words
+dfcorpus = tm_map(dfcorpus, removePunctuation)
+dfcorpus = tm_map(dfcorpus, removeNumbers)
+dfcorpus = tm_map(dfcorpus, removeWords, stopwords("english"))
+dfcorpus = tm_map(dfcorpus, stripWhitespace)
+
+#creating document term matrix
+df_dtm = DocumentTermMatrix(dfcorpus)
+dtm.matrix = as.matrix(df_dtm)
 
 #Creating Features / key words and phrases that suggest actionability
-df$shall = str_detect(df[,3],"shall")
-df$shall_submit = str_detect(df[,3],"shall submit")
-df$permittee_shall = str_detect(df[,3],"permittee shall")
-df$permittee_must = str_detect(df[,3],"permittee must")
-df$owner_or_operator = str_detect(df[,3],"owner or operator")
-df$owner_operator = str_detect(df[,3],"owner/operator")
+# df$shall = str_detect(df[,3],"shall")
+# df$shall_submit = str_detect(df[,3],"shall submit")
+# df$permittee_shall = str_detect(df[,3],"permittee shall")
+# df$permittee_must = str_detect(df[,3],"permittee must")
+# df$owner_or_operator = str_detect(df[,3],"owner or operator")
+# df$owner_operator = str_detect(df[,3],"owner/operator")
 
 #Subsetting separating categories
-dfActionable = subset(dfTr, Category == "Actionable") 
-dfDef = subset(dfTr, Category == "NA - Definition") 
-dfOther = subset(dfTr, Category == "NA - Other") 
-dfTrig = subset(dfTr, Category == "Trigger") 
-dfUnCat = subset(dfTr, Category == "")
+# dfActionable = subset(dfTr, Category == "Actionable") 
+# dfDef = subset(dfTr, Category == "NA - Definition") 
+# dfOther = subset(dfTr, Category == "NA - Other") 
+# dfTrig = subset(dfTr, Category == "Trigger") 
+# dfUnCat = subset(dfTr, Category == "")
